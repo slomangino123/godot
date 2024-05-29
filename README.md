a6f95ac4cec436e0e70da948059c6b79523ea444
slomangino <123slomangino@gmail.com>
2024-05-29 09:22:00 -0400

    test out some terrain and collisions and y-sorting

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # godot
-a place for my games with godot
+This is a place for my games created with Godot
+
+### Resources
+[Brackey's channel](https://www.youtube.com/@Brackeys)
+[y-sorting and tilemap collisions](https://www.youtube.com/watch?v=GiZuWjXmvcc&ab_channel=DevWorm)
+
--- turn-based-rpg/scripts/player.gd
+++ turn-based-rpg/scripts/player.gd
@@ -4,5 +4,7 @@ extends CharacterBody2D
 const SPEED = 120.0
 
 func _physics_process(delta):
-	position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized() * SPEED * delta
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	velocity = direction * SPEED
+	move_and_slide()
 	
